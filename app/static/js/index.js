db614e1abdf0c64ce8f9fdbde62cc710d6d36678
TsouMinHan <p87089905@gmail.com>
2021-03-29 20:49:31 +0800

    取得日記資料

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,6 +1,7 @@
 
 let data = {
-    title: "daily"
+    title: "daily",
+    dailys: []
 }
 
 var app = new Vue({
@@ -8,19 +9,46 @@ var app = new Vue({
     delimiters: ["[[", "]]"],
     data: data,
     mounted: function () {
-
+        this.loadData();
     },
     methods: {
         submitData: function () {
             const data = {
-                content: document.querySelector("#editor").value,
+                content: CKEDITOR.instances["editor"].getData(),
                 timestamp: Math.round(Date.now()/1000) // now 得到的是毫秒，再除 1000 變成秒數
             }
             let xhr = new XMLHttpRequest();
 
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == XMLHttpRequest.DONE) {
+                    const results = JSON.parse(xhr.responseText);
+                    
+                    if (results.status == "success"){
+                        console.log(results.status)
+                        CKEDITOR.instances["editor"].setData(""); // clear textarea
+                    }
+                }
+            }
+
             xhr.open("post", "/api/save_daily", true);
             xhr.setRequestHeader('Content-type', 'application/json');
             xhr.send(JSON.stringify(data));
+        },
+        loadData: function () {
+            let xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == XMLHttpRequest.DONE) {
+                    const results = JSON.parse(xhr.responseText);
+                    
+                    if (results.status == "success"){
+                        data.dailys = results.dailys;
+                    }
+                }
+            }
+
+            xhr.open("post", "/api/load_daily", true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.send({})
         }
     }
 });
--- app/view/daily.py
+++ app/view/daily.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify, render_template, url_for
+from flask import request, jsonify, render_template, url_for, session
 
 from datetime import datetime
 
@@ -7,17 +7,29 @@ from app.model import MainDB
 
 db = MainDB()
 
+@main.route("/api/load_daily", methods=["GET", "POST"])
+def load_daily():
+    res = {
+        "status": "success",
+        "dailys": db.read()
+        }
+    return jsonify(res)
+
 @main.route("/api/save_daily", methods=["GET", "POST"])
 def save_daily():
     data = request.get_json()
     content = data["content"]
     timestamp = data["timestamp"]
     
-    db.append(content, datetime.fromtimestamp(timestamp))
+    try:
+        db.append(content, datetime.fromtimestamp(timestamp))
+        res = {"status": "success"}
+    except:
+        res = {"status": "failed"}
 
-    return jsonify(data)
+    return jsonify(res)
 
 @main.route("/", methods=["GET"])
 @main.route("/daily", methods=["GET"])
-def daily_index():
+def daily_index():    
     return render_template("index.html")
