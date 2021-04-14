a3e160f0821bda7f25d733f092f72bc18c4e18fe
TsouMinHan <p87089905@gmail.com>
2021-04-14 19:13:47 +0800

    正常顯示當月份前後的日期
    修改日曆資料格式

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -19,20 +19,17 @@ var app = new Vue({
     computed: {
         DayArr: function() {
             // month - 1 是因為 month 我有給他 + 1
-            const first = new Date(this.Calendar.year, this.Calendar.month - 1, 1);
-            const last = new Date(this.Calendar.year, this.Calendar.month, 0);
-            // const first = new Date(2021, 5 - 1, 1);
-            // const last = new Date(2021, 5, 0);
+            const firstDateOfMonth = new Date(this.Calendar.year, this.Calendar.month - 1, 1);
+            const lastDateOfMonth = new Date(this.Calendar.year, this.Calendar.month, 0);
 
-            const firstDate = first.getDate();
-            const lastDate = last.getDate(); // 代表 這個月份的前一天
-
-            const firstDay = first.getDay();
-            const lastDay = last.getDay();
+            const firstDay = firstDateOfMonth.getDay(); // 星期幾 0-6
+            const lastDate = lastDateOfMonth.getDate(); // 幾號
 
             // 日曆上個月分的尾段日期，從星期幾開始
+            const lastDateOfPreMonth = new Date(this.Calendar.year, this.Calendar.month - 1, 0);
             let tmp = [];
-            for (i = 0; i < firstDay; i++) {
+
+            for (i = lastDateOfPreMonth.getDate() - firstDay + 1; i <= lastDateOfPreMonth.getDate(); i++) {
                 tmp.push(i);
             }
 
@@ -42,15 +39,28 @@ var app = new Vue({
             }
 
             // 日曆上補上下個月的前段日期
-            for (i = 0; i < 7 * 5 - tmp.length; i++) {
+            for (i = 1; i <= 7 * 5 - tmp.length; i++) {
                 tmp.push(i);
             }
 
             let output = {};
             // 放入 dayArr 中
             for (i = 0; i < 5; i++) {
-                output[i] = tmp.slice(i * 7, (i + 1) * 7);
+                output[i] = [];
+
+                tmp.slice(i * 7, (i + 1) * 7).forEach(day => {
+                    output[i].push({
+                        "day": day,
+                        "article": ""
+                    });
+                })
+
             }
+            // output = {
+            //     "0": [
+            //         { "day": 1, "article": "" }, ... * 7
+            //     ], ...
+            // }
 
             return output;
         }
